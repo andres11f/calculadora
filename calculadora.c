661da7ff0d8a940fcea447a8e4b290eaf7214043
andres11f <andres11f@hotmail.com>
2014-04-02 13:41:39 -0500

    Create calculadora.c

---
+++ calculadora.c
@@ -0,0 +1,8 @@
+int main (){
+	int op1 = 4;
+	int op2 = 3;
+	printf ("SUMA: %i \n", suma(op1, op2));
+	printf ("RESTA: %i \n", resta(op1, op2));
+	printf ("MULTIPLICACION: %i \n", multiplicacion(op1, op2));
+	return 0;
+}
